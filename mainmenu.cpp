55ab5198eb273d49ecb4bb0b591b31ee976eea72
chaudhry_0972952 <0972952@my.scccd.edu>
2025-05-08 14:37:24 -0700

    Ridah Chaudhry - added russian roulette and outputs warning if balance < 50

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -3,24 +3,66 @@
 #include <vector>
 using namespace std;
 
+int starting_balance = 55; //placeholder for testing
+
 void init_colors() {
 	start_color();
-	init_pair(1, COLOR_WHITE, COLOR_RED);	//starting screen
-	init_pair(2, COLOR_GREEN, COLOR_BLACK);	//main menu
-	init_pair(3, COLOR_BLACK, COLOR_YELLOW);	//games screen placeholder
+	init_pair(1, COLOR_WHITE, COLOR_BLUE);		//starting screen
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);		//main menu
+	init_pair(3, COLOR_RED, COLOR_BLACK);		//blackjack starting screen
+	init_pair(4, COLOR_YELLOW, COLOR_BLACK);	//ride the bus starting screen
+	init_pair(5, COLOR_CYAN, COLOR_BLACK);		//slot machine starting screen
+	init_pair(6, COLOR_MAGENTA, COLOR_BLACK);	//russian roulette starting screen
 	
 }
 void start_screen() {
 	clear();
 	bkgd(COLOR_PAIR(1));
-	mvprintw(LINES / 2 -2, (COLS - 20) / 2, "WELCOME TO GEEBO'S GAMBLING CASINO!");
+	mvprintw(LINES / 2 -2, (COLS - 20) / 2, "WELCOME TO GEEBO'S GAMBLING BONANZA!");
 	mvprintw(LINES / 2, (COLS - 25) / 2, "Press any key to continue...");
+	refresh();
+	getch();
+}
+void blackjack_screen() {
+	clear();
+	bkgd(COLOR_PAIR(3));
+	mvprintw(LINES / 2 -2, (COLS - 12) / 2, "WELCOME TO BLACKJACK");
+	mvprintw(LINES / 2, (COLS - 30) / 2, "Press any key to continue...");
+	refresh();
+	getch();
+}
+void ridethebus_screen() {
+	clear();
+	bkgd(COLOR_PAIR(4));
+	mvprintw(LINES / 2 -2, (COLS - 14) / 2, "WELCOME TO RIDE THE BUS");
+	mvprintw(LINES / 2, (COLS - 30) / 2, "Press any key to continue...");
+	refresh();
+	getch();
+}
+void slots_screen() {
+	clear();
+	bkgd(COLOR_PAIR(5));
+	mvprintw(LINES / 2 -2, (COLS - 13) / 2, "WELCOME TO SLOTS");
+	mvprintw(LINES / 2, (COLS - 30) / 2, "Press any key to continue...");
+	refresh();
+	getch();
+}
+void roulette_screen() {
+	clear();
+	bkgd(COLOR_PAIR(6));
+	if (starting_balance < 50) {
+	mvprintw(LINES / 2 -2, (COLS - 20) / 2, "INSUFFICIENT FUNDS - OBTAIN $50 TO PLAY");
+	mvprintw(LINES / 2, (COLS - 25) / 2, "Press any key to continue...");
+	}else {
+	mvprintw(LINES / 2 -2, (COLS - 15) / 2, "WELCOME TO RUSSIAN ROULETTE");
+	mvprintw(LINES / 2, (COLS - 30) / 2, "Press any key to continue...");
+	}
 	refresh();
 	getch();
 }
 
 void main_menu() {
-	vector<string> options = {"1. BlackJack", "2. Ride the Bus", "3. Slots" , "4. Exit"};
+	vector<string> options = {"1. BlackJack", "2. Ride the Bus", "3. Slots" ,"4. Russian Roulette", "5. Exit"};
 	int choice = 0;
 	int ch;
 
@@ -48,18 +90,18 @@ void main_menu() {
 			choice = (choice == options.size() - 1) ? 0 : choice + 1;
 			break;
 
-			case 10: //enter key
-			if (choice == options.size() -1) {
+			case 10: //user enter key
+			if (choice == 0) {
+				blackjack_screen();
+			} else if (choice == 1) {
+				ridethebus_screen();
+			} else if (choice == 2) {
+				slots_screen();
+			} else if (choice == 3) {
+				roulette_screen();
+			} else if (choice == 4) {
 				return;
-			} else {
-				//Placeholder for game screens
-					clear();
-					bkgd(COLOR_PAIR(3));
-					mvprintw(LINES / 2, (COLS - 30) / 2, "You selected: %s", options[choice].c_str());
-					mvprintw(LINES / 2 + 2, (COLS - 30) / 2, "Press any key to return...");
-					refresh();
-					getch();
-				}
+			}
 			break;
 		}
 	}
