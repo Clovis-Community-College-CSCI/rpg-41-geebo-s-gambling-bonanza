fb097a56a4adc0c35d125302b341df8d792589cb
chaudhry_0972952 <0972952@my.scccd.edu>
2025-05-14 12:58:19 -0700

    Ridah: added some more visuals to map

--- gamemap.h
+++ gamemap.h
@@ -29,20 +29,44 @@ int main (void) {
 	if( (c = newwin(halfy, halfx, halfy, 0)) == NULL) bomb();
 	if( (d = newwin(halfy, halfx, halfy, halfx)) == NULL) bomb();
 
-	mvwaddstr(a,0,0, "Game 1: BLACKJACK\n");						//writing to each window
 	wbkgd(a, COLOR_PAIR(1));										//assigning color pair to each window
+	mvwaddstr(a,0,0, "Game 1: BLACKJACK\n");						//writing to each window
+	mvwaddstr(a,2,2, "_____ _____");					
+	mvwaddstr(a,3,2, "|A  . | |K   |");					
+	mvwaddstr(a,4,2, "|  / . \\  |  |     |");					
+	mvwaddstr(a,5,2, "| (_._) |  |      |");					
+	mvwaddstr(a,6,2, "|   |   |  |  ♠️  |");					
+	mvwaddstr(a,7,2, "|____V| |_____|");					
 	wrefresh(a);
 	
-	mvwaddstr(b,0,0, "Game 2: RIDE THE BUS\n");
 	wbkgd(b, COLOR_PAIR(2));
+	mvwaddstr(b,0,0, "Game 2: RIDE THE BUS\n");
+	mvwaddstr(a,2,2, "  ______");					
+	mvwaddstr(a,3,2, " /      \\");					
+	mvwaddstr(a,4,2, "|  BUS   |");					
+	mvwaddstr(a,5,2, "|______|");										
+	mvwaddstr(a,7,2, "Get ready to play!");										
 	wrefresh(b);
 
-	mvwaddstr(c,0,0, "Game 3: SLOTS\n");
 	wbkgd(c, COLOR_PAIR(3));
+	mvwaddstr(c,0,0, "Game 3: SLOTS\n");
+	mvwaddstr(a,2,2, " _______ ");					
+	mvwaddstr(a,3,2, "| 7  |  🍒  |");					
+	mvwaddstr(a,4,2, "| BAR |  7 |");					
+	mvwaddstr(a,5,2, "| 🍒 |  BAR |");					
+	mvwaddstr(a,6,2, " --------");					
+	mvwaddstr(a,8,2, "Get ready to spin!");					
 	wrefresh(c);
 
-	mvwaddstr(d,0,0, "Game 4: RUSSIAN ROULETTE - MUST ACQUIRE $50 TO PLAY\n");
 	wbkgd(d, COLOR_PAIR(4));
+	mvwaddstr(d,0,0, "Game 4: RUSSIAN ROULETTE - MUST ACQUIRE $50 TO PLAY\n");
+	mvwaddstr(a,2,2, "   _____________ ");					
+	mvwaddstr(a,3,2, "   |  REVOLVER  |");					
+	mvwaddstr(a,4,2, "   |____________| ");					
+	mvwaddstr(a,5,2, "        | | ");					
+	mvwaddstr(a,6,2, "        (oo)");					
+	mvwaddstr(a,7,2, "     --/----\\--");					
+	mvwaddstr(a,9,2, "WARNING: $50 TO PLAY!");					
 	wrefresh(d);
 	getchar();
 
--- main.cc
+++ main.cc
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "playerclass.h"
+#include "gamemap.h"
 #include "gamefuncs.cc"
 using namespace std;
 
 int main(){
 	//Add the game running stuff here
+	game_map();
+
 	return 0;
 }
